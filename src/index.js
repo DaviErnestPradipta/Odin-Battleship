b86f3cd4ec170b3a84bad919d073992cfdaa5734
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-05 17:09:21 +0900

    Add barebones HTML/CSS/JS stack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function createBoard(containerSelector) {
+    const board = document.querySelector(containerSelector);
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        board.appendChild(cell);
+    }
+}
+
+createBoard('.player .board');
+createBoard('.computer .board');

5cadd9bd675fe3dff961c6b04cb8c81988a46c41
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-05 16:25:17 +0900

    Add Gameboard and test

---
+++ src/Gameboard.js
@@ -0,0 +1,45 @@
+import Ship from "./Ship.js";
+
+const BOARD_SIZE = 10;
+
+export default class Gameboard {
+    constructor() {
+        this.board = Array(BOARD_SIZE).fill(null).map(() => Array(BOARD_SIZE).fill(null));
+        this.ships = [];
+        this.missedAttacks = [];
+    }
+
+    placeShip(length, x, y, isVertical = false) {
+        const ship = new Ship(length);
+        const positions = [];
+
+        for (let i = 0; i < length; i++) {
+            const row = isVertical ? y + i : y;
+            const col = isVertical ? x : x + i;
+
+            if (row >= BOARD_SIZE || col >= BOARD_SIZE || this.board[row][col] !== null)
+                throw new Error("Invalid ship placement");
+
+            positions.push([row, col]);
+        }
+
+        positions.forEach(([row, col]) => this.board[row][col] = ship);
+        this.ships.push({ship, positions, hits: []});
+    }
+
+    receiveAttack(x, y) {
+        const cell = this.board[y][x];
+
+        if (cell instanceof Ship) {
+            cell.hit();
+            const shipData = this.ships.find(s => s.ship === cell);
+            shipData.hits.push([y, x]);
+        }
+
+        else this.missedAttacks.push([y, x]);
+    }
+
+    allShipsSunk() {
+        return this.ships.every(({ship}) => ship.isSunk());
+    }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,5 @@
-import Ship from "./ship.js";
+import Ship from "./Ship.js";
+import Gameboard from "./Gameboard.js";
 
 test('Sink destroyer after three hits', () => {
     const destroyer = new Ship(3);
@@ -7,4 +8,24 @@ test('Sink destroyer after three hits', () => {
     expect(destroyer.isSunk()).toBeFalsy();
     destroyer.hit();
     expect(destroyer.isSunk()).toBeTruthy();
+})
+
+test('Place a ship and sink it', () => {
+    const board = new Gameboard();
+    board.placeShip(3, 0, 0);
+
+    board.receiveAttack(0, 0);
+    board.receiveAttack(1, 0);
+    board.receiveAttack(2, 0);
+
+    expect(board.allShipsSunk()).toBeTruthy();
+})
+
+test('Records missed attacks', () => {
+    const board = new Gameboard();
+    board.placeShip(2, 0, 0);
+
+    board.receiveAttack(5, 5);
+
+    expect(board.missedAttacks).toContainEqual([5, 5]);
 })

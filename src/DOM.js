f19315c03bab017c826f456bfcd5b7d1ada27c59
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-05 17:35:50 +0900

    Add DOM.js

---
+++ src/DOM.js
@@ -0,0 +1,7 @@
+export function createBoard(containerSelector) {
+    const board = document.querySelector(containerSelector);
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        board.appendChild(cell);
+    }
+};

6cf6cb91e3532150fee16713c7059c64f4c2a6c9
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-06 09:06:36 +0900

    Factor revealShip outside of createShip

--- src/helper/DOM.js
+++ src/helper/DOM.js
@@ -1,5 +1,13 @@
 import {BOARD_SIZE} from "./Constants.js";
 
+const letterMap = {
+    5: 'A',
+    4: 'B',
+    3: 'C',
+    2: 'D',
+    1: 'S'
+};
+
 export function createBoard(containerSelector) {
     const board = document.querySelector(containerSelector);
     for (let i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
@@ -9,34 +17,23 @@ export function createBoard(containerSelector) {
 };
 
 export function createShip(length, x, y, isVertical = false, boardSelector) {
+    const reveal = boardSelector.includes('human');
+    if (reveal) revealShip(length, x, y, isVertical, boardSelector);
+    updateTracker(letterMap[length], boardSelector);
+}
+
+function revealShip(length, x, y, isVertical = false, boardSelector) {
     const board = document.querySelector(boardSelector);
 
-    const letterMap = {
-        5: 'A',
-        4: 'B',
-        3: 'C',
-        2: 'D',
-        1: 'S'
-    };
+    for (let i = 0; i < length; i++) {
+        const row = isVertical ? y + i : y;
+        const col = isVertical ? x : x + i;
 
-    const shipLetter = letterMap[length];
-    if (!shipLetter) return;
+        const cellIndex = row * BOARD_SIZE + col;
+        const cell = board.children[cellIndex];
 
-    const reveal = boardSelector.includes('human');
-
-    if (reveal) {
-        for (let i = 0; i < length; i++) {
-            const row = isVertical ? y + i : y;
-            const col = isVertical ? x : x + i;
-    
-            const cellIndex = row * BOARD_SIZE + col;
-            const cell = board.children[cellIndex];
-    
-            if (cell) cell.textContent = shipLetter;
-        }
+        if (cell) cell.textContent = letterMap[length];
     }
-
-    updateTracker(shipLetter, boardSelector);
 }
 
 function updateTracker(letter, boardSelector) {
